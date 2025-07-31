ba8aa7dd9d23c3af3be3e1c947940c42caf84629
Ubeyid Oumer <ubeyidah@gmail.com>
2025-07-31 10:29:38 +0300

    feat: add css varible color and fonts

---
+++ client/app/fonts.ts
@@ -0,0 +1,11 @@
+import { DM_Sans, Roboto } from "next/font/google";
+
+export const dmSans = DM_Sans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+});
+
+export const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
