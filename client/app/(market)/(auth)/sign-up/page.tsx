9113e5ae6cd86027404049a49afaa1e2729c9b50
Ubeyid Oumer <ubeyidah@gmail.com>
2025-07-31 14:43:26 +0300

    feat: sign up layout

---
+++ client/app/(market)/(auth)/sign-up/page.tsx
@@ -0,0 +1,38 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import SignInForm from "../_components/sign-in-form";
+
+const page = () => {
+  return (
+    <Card className="w-full max-w-lg px-3">
+      <CardHeader>
+        <CardTitle className="text-center text-2xl">
+          Create your LinkMint account
+        </CardTitle>
+        <CardDescription className="text-center">
+          Start shortening smarter – get free coins when you join.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <SignInForm />
+      </CardContent>
+      <CardFooter className="block">
+        <p className="text-center text-sm">
+          Already have an account?{" "}
+          <Link href="/sign-in" className="text-primary hover:underline">
+            Sign In
+          </Link>
+        </p>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default page;
