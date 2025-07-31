f6379ac798c20cd2dfb79a3fbddf32937cc68def
Ubeyid Oumer <ubeyidah@gmail.com>
2025-07-31 18:50:50 +0300

    feat(api):stup basic express server

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,1 +1,8 @@
-console.log(0);
+import express from "express";
+import { env } from "envgaurd";
+const app = express();
+
+const port = env("PORT", 8080);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
