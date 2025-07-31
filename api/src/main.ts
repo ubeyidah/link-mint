19ed386085c6c90738df9c85ce23fc2a4ba53e35
Ubeyid Oumer <ubeyidah@gmail.com>
2025-07-31 19:56:29 +0300

    feat(api): add cors middleware

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,8 +2,15 @@ import express from "express";
 import { env } from "envgaurd";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
+import cors from "cors";
 const app = express();
-
+app.use(
+  cors({
+    origin: [env("BETTER_AUTH_URL") as string],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 // routes
 app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
