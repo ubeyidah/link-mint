35ff8a2da24ec0e28bfc94da1f21e6502a0895fb
Ubeyid Oumer <ubeyidah@gmail.com>
2025-07-31 19:22:33 +0300

    feat(api): setup better auth for the express server

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import { env } from "envgaurd";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 const app = express();
+
+// routes
+app.all("/api/auth/*splat", toNodeHandler(auth));
+app.use(express.json());
 
 const port = env("PORT", 8080);
 app.listen(port, () => {
