ac31d5a9988bb6da91d87a38168311eb798c79b2
Ubeyid Oumer <ubeyidah@gmail.com>
2025-07-31 14:54:27 +0300

    feat: sign up validation

--- client/validation/auth.validation.ts
+++ client/validation/auth.validation.ts
@@ -9,3 +9,21 @@ export const signInSchema = z.object({
 });
 
 export type SignInType = z.infer<typeof signInSchema>;
+
+export const signUpSchema = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Name must be at least 2 characters long" })
+    .max(50, { message: "Name must be less than 50 characters" })
+    .regex(/^[a-zA-Z\s'-]+$/, {
+      message:
+        "Name can only contain letters, spaces, apostrophes, and hyphens",
+    }),
+  email: z.string().email("Invalid email address"),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters long")
+    .max(128, "Password must not exceed 128 characters"),
+});
+
+export type SignUpType = z.infer<typeof signUpSchema>;
